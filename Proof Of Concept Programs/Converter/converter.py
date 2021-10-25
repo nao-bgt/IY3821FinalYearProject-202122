128114acbe490f66416fef0d4482149004d4fdf3
nao-bgt <naomi.bagot@outlook.fr>
2021-10-25 17:50:46 +0000

    Creation of Class Converter and Unit Test Case for object Converter.

---
+++ Proof Of Concept Programs/Converter/converter.py
@@ -0,0 +1,6 @@
+
+
+class Converter:
+
+    def __init__(self):
+        pass
---
+++ Proof Of Concept Programs/Converter/testconverter.py
@@ -0,0 +1,8 @@
+import unittest
+
+from converter import Converter
+
+class TestConverter(unittest.TestCase):
+
+    def setUp(self):
+        self.converter = Converter()
