d271b24bdc0634aa24f309feac687cdf8b1855e9
nao-bgt <naomi.bagot@outlook.fr>
2021-11-11 03:24:36 +0000

    Creation of method GetBinData and Unit Test Case for method.

--- Proof Of Concept Programs/Converter/converter.py
+++ Proof Of Concept Programs/Converter/converter.py
@@ -4,3 +4,13 @@ class Converter:
 
     def __init__(self):
         pass
+
+    def getBinaryData(self, filename):
+        
+        binary_data = []
+
+        with open(filename, 'rb') as file:
+            while (byte := file.read(1)):
+                binary_data.append(ord(byte))
+        
+        return binary_data
